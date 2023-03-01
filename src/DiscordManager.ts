8756a52ad949d60c9c17a8b998f3ef0647e005e0
CodedByDopi <67605541+DopiGFX@users.noreply.github.com>
2023-03-01 20:29:37 +0100

    added types for webhook and fixed bug with timestamp

--- src/DiscordManager.ts
+++ src/DiscordManager.ts
@@ -2,18 +2,28 @@ import dayjs from 'dayjs';
 import got from 'got';
 
 type BodyType = "stopped" | "started" | "refused_connection"
+type SimpleDcWebhookObject = {
+  content: null;
+  embeds: SimpleDcWebhookEmbed[];
+  attachments: any[];
+}
+type SimpleDcWebhookEmbed = {
+  title: string;
+  description: string;
+  color: number;
+  timestamp?: string;
+}
 
 export class DiscordManager {
 
-  public typeToBody: Map<BodyType, Object> = new Map([
+  public typeToBody: Map<BodyType, SimpleDcWebhookObject> = new Map([
     ["stopped", {
       content: null,
       embeds: [
         {
           title: "Server offline!",
           description: "Der Server wurde gestoppt oder ist gecrashed!",
-          color: 16711680,
-          timestamp: dayjs().toISOString()
+          color: 16711680
         }
       ],
       attachments: []
@@ -24,8 +34,7 @@ export class DiscordManager {
         {
           title: "Server online!",
           description: "Der Server ist gestartet!",
-          color: 4718336,
-          timestamp: dayjs().toISOString()
+          color: 4718336
         }
       ],
       attachments: []
@@ -36,8 +45,7 @@ export class DiscordManager {
         {
           title: "Server crashed!",
           description: "Der Server hat die Verbindung abgelehnt!",
-          color: 16711680,
-          timestamp: dayjs().toISOString()
+          color: 16711680
         }
       ],
       attachments: []
@@ -48,6 +56,8 @@ export class DiscordManager {
 
   public async send(type: BodyType): Promise<void> {
     const apiUrl = "https://discord.com/api/webhooks/" + process.env.dcCannelId + "/" + process.env.dcWebghookToken;
+    let body = this.typeToBody.get(type);
+    body.embeds.map(e => e.timestamp = dayjs().toISOString())
     await got.post(apiUrl, {
       json: this.typeToBody.get(type)
     }).json();
